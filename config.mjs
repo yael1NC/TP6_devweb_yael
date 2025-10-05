321ab18293fa7a2d4efabb494ba25ab2cdd23558
yael1NC <yael.avelot@gmail.com>
2025-10-05 14:35:13 +1100

    Partie 2: API v1 complète et fonctionnelle

--- config.mjs
+++ config.mjs
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: parseInt(process.env.PORT) || 8080,
+  port: process.env.PORT || 8080,
   linkLen: parseInt(process.env.LINK_LEN) || 6,
   dbFile: process.env.DB_FILE || 'database/database.sqlite',
   dbSchema: process.env.DB_SCHEMA || 'database/database.sql',
